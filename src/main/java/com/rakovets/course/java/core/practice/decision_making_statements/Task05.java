d7fc4701a706644e84b7f2d849abf6f49905724a
Vladislav Mitskevich <mitskevichvlm@gmail.com>
2022-12-25 20:03:35 +0300

    feat: add solution with else-if statement for Task04 on conditional constructions

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task05.java
@@ -1,5 +1,6 @@
 package com.rakovets.course.java.core.practice.decision_making_statements;
 
+
 /**
  * Разработать программу для игрового движка.
  * <p>
@@ -31,8 +32,34 @@ class Task05 {
      * @return месяц на английском языке (UPPER CASE)
      */
     static String getMonthName(byte monthNumber) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String nameMonth;
+        if (monthNumber == 1) {
+            nameMonth = "JANUARY";
+        } else if (monthNumber == 2) {
+            nameMonth = "FEBRUARY";
+        } else if (monthNumber == 3) {
+            nameMonth = "MARCH";
+        } else if (monthNumber == 4) {
+            nameMonth = "APRIL";
+        } else if (monthNumber == 5) {
+            nameMonth = "MAY";
+        } else if (monthNumber == 6) {
+            nameMonth = "JUNE";
+        } else if (monthNumber == 7) {
+            nameMonth = "JULY";
+        } else if (monthNumber == 8) {
+            nameMonth = "AUGUST";
+        } else if (monthNumber == 9) {
+            nameMonth = "SEPTEMBER";
+        } else if (monthNumber == 10) {
+            nameMonth = "OCTOBER";
+        } else if (monthNumber == 11) {
+            nameMonth = "NOVEMBER";
+        } else if (monthNumber == 12) {
+            nameMonth = "DECEMBER";
+        } else {
+            nameMonth = null;
+        }
+        return nameMonth;
     }
 }
