97436286c728f613ae036676bc941685f6c83167
Vladislav Mitskevich <mitskevichvlm@gmail.com>
2023-01-14 21:31:59 +0300

    feat: add solution for Task03 on Rectangle

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/task03_rectangle/PointToRectangle.java
@@ -0,0 +1,27 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects.task03_rectangle;
+
+public class PointToRectangle {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public PointToRectangle(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
