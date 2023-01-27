d4d71adb89f6723440c75ca0b90451cf939bceb2
Vladislav Mitskevich <mitskevichvlm@gmail.com>
2023-01-27 23:33:06 +0300

    feat: add solution on Warrior

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Demonstration.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Demonstration.java
@@ -1,15 +1,21 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
 
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.enemy.Golem;
 import com.rakovets.course.java.core.practice.oop_principles.battle_ground.enemy.RedDragon;
 import com.rakovets.course.java.core.practice.oop_principles.battle_ground.enemy.Zombie;
 import com.rakovets.course.java.core.practice.oop_principles.battle_ground.hero.Archer;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.hero.Mag;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.hero.Warrior;
 
 public class Demonstration {
     public static void main(String[] args) {
         Archer archer = new Archer("Legolas");
         Zombie zombie = new Zombie();
         RedDragon rd = new RedDragon();
-        archer.attack(zombie);
+        Mag magus = new Mag("Pendalf");
+        Golem golem = new Golem();
+        Warrior warrior = new Warrior("Aragorn");
+        warrior.attack(golem);
 
     }
 }
--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/hero/Warrior.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/hero/Warrior.java
@@ -1,24 +1,56 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground.hero;
 
 import com.rakovets.course.java.core.practice.oop_principles.battle_ground.DamageType;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.DiceRoll;
 import com.rakovets.course.java.core.practice.oop_principles.battle_ground.Fighter;
 
 public class Warrior extends Hero {
     private final static DamageType[] damageTypes = {DamageType.PHYSICAL, DamageType.MELEE};
-
+    private final static int ARMOR_CLASS = 21;
+    private final static int WARRIOR_HEALTH = 320;
 
 
     public Warrior(String name) {
-        super(name, damageTypes, 400);
+        super(name, damageTypes, WARRIOR_HEALTH);
+    }
+
+    @Override
+    public void attack(Fighter enemy) {
+        System.out.println(this.toString() + " attaks " + enemy.toString());
+        enemy.takeDamage(this);
+    }
+
+    @Override
+    public void takeDamage(Fighter enemy) {
+        int enemyAttack = DiceRoll.roll20()+5;
+        if (enemyAttack >= ARMOR_CLASS) {
+            super.takeDamage(enemy);
+        } else {
+            System.out.println(this.getName() +" blocked attack");
+        }
+        if (isAlive()) {
+            attack(enemy);
+        } else {
+            System.out.println(enemy + " killed archer" + this.getName());
+        }
     }
 
     @Override
     public int getDamageAmount() {
-        return 0;
+        countHit();
+        int damageAmount;
+        if (getCount() % 3 == 0) {
+            damageAmount = DiceRoll.roll10() * 3;
+            System.out.println(this.getName() + " has power attack");
+        } else {
+            damageAmount = DiceRoll.roll10();
+            System.out.println("Attack");
+        }
+        return damageAmount;
     }
 
     @Override
-    public void attack(Fighter fighter) {
-
+    public String toString() {
+        return getName();
     }
 }
